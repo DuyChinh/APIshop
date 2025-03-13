bfde00be4dc7d0070ac55885340209b2251dd930
DuyChinh <90370996+DuyChinh@users.noreply.github.com>
2025-03-13 12:02:42 +0700

    Update auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,13 +1,14 @@
 module.exports = (req, res, next) => {
-  const pathName = req.baseUrl + req.path;
-  console.log(req.user, pathName);
-    if (req.user && pathName === "/admin/login") {
-      return res.redirect("/admin");
-    }
+  // const pathName = req.baseUrl + req.path;
+  // console.log(req.user, pathName);
+  //   if (req.user && pathName === "/admin/login") {
+  //     return res.redirect("/admin");
+  //   }
 
-    if (req.user ||  pathName === "/admin/login") {
-        return next();
-    }
+  //   if (req.user ||  pathName === "/admin/login") {
+  //       return next();
+  //   }
   
-    return res.redirect("/admin/login");
+  //   return res.redirect("/admin/login");
+  next();
 };
